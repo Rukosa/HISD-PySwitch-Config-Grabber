227502f575003a2234d4159040be57e8cc47141c
MICHAEL MCGINNIS <Michael.McGinnis@HISD.org>
2025-01-29 12:39:20 -0600

    Added Serial Feature/Error handling/Menu options
    Generalized error handling for netmiko exceptions. An untested serial feature also added. *Should* work assuming the switchdetails are correct.
    Menu logic has been fixed up a little bit and reflects the added serial feature.

--- off_switch_functions.py
+++ off_switch_functions.py
@@ -1,12 +1,16 @@
+#Menu options to get details and fill in what is needed for the switch
+#logic is a bit complex..
 from on_switch_functions import *
 import os
+
 
 #Menu options    
 def grab_configs_opt(choice=0):
     if choice == 0:
         print('''
-1. Grab one config
-2. Grab many configs
+1. Grab one config (Network)
+2. Grab many configs (Network)
+3. Grab one config (Serial) 
         ''')
         choice = input("#")
     #one
@@ -25,21 +29,38 @@ def grab_configs_opt(choice=0):
         else:
             grab_configs_opt(choice)
             return
+    elif choice == '3':
+        grabconfigip('serial')
     else:
         print("Invalid input...")
         grab_configs_opt()
-    
-def dump_config_opt(switch_ip=0, dump_txt=0):
+
+#Built to have reset points. Reset to a certain point by filling in a value with 0 or restart with a blank function call
+def dump_config_opt(switch_ip=0, dump_txt=0, serial_or_network=0):
+    #Serial/Network logic
+    if serial_or_network == 0:
+        print('''
+    1. Serial
+    2. Network
+            ''')
+        serial_or_network = input('#')
+    if serial_or_network == '1':
+        switch_ip = 'serial'
+    elif not (serial_or_network == '2') and not (serial_or_network == '1'):
+        print("Invalid input")
+        dump_config_opt()
+        return
+    #Switch IP and Text logic. Switch IP assumes network was chosen
     if switch_ip == 0:
         switch_ip = input("IP of switch: ")
         if not check_ip(switch_ip):
             print("IP is invalid...")
-            dump_config_opt()
+            dump_config_opt(0, 0, serial_or_network)
             return
     if dump_txt == 0:
         dump_txt = input("Name of config txt file [no file extension!]: ")
         if not check_txt_exists(dump_txt):
-            dump_config_opt(switch_ip)
+            dump_config_opt(switch_ip, 0, serial_or_network)
             return
     
     confirm_input = input(f"Are you sure you want to dump to {switch_ip} with {dump_txt}.txt? (y/n): ")
@@ -49,7 +70,7 @@ def dump_config_opt(switch_ip=0, dump_txt=0):
         dump_config_opt()
     else:
         print("Invalid input")
-        dump_config_opt(switch_ip, dump_txt)
+        dump_config_opt(switch_ip, dump_txt, serial_or_network)
         
 def check_txt_exists(txt_file):
     if os.path.exists(txt_file + ".txt"):
--- on_switch_functions.py
+++ on_switch_functions.py
@@ -1,13 +1,19 @@
 from netmiko import ConnectHandler
 import ipaddress   
 import time
+import getpass
 
 #Returns the connection details of a switch given the IP.
-def switch_connect(switch_ip):
-    print("Username: ")
-    username = input()
-    print("Password: ")
-    password = input()
+def switch_connect(switch_ip, username="", password=""):
+    if username == "" and password == "":
+        username = input("Username: ")
+        try:
+            password = getpass.getpass()
+        except Exception as err:
+            print("--------------------------")
+            print(err)
+            print("--------------------------")
+            
     '''
     Example of switch input for netmiko:
 
@@ -20,13 +26,27 @@ def switch_connect(switch_ip):
     }
     '''
     
-    switch_details = {
+    if switch_ip == 'serial':
+        port = input("Serial line: ")
+        switch_details = {
+            'device_type' : 'cisco_ios_serial',
+            'port' : port,
+            'username' : username,
+            'password' : password,
+            'serial_settings' : {
+                'baudrate' : 9600,
+                'timeout' : 15,
+                },
+        }
+    else:
+        switch_details = {
         'device_type' : 'cisco_ios',
         'host' : switch_ip,
         'username' : username,
         'password' : password,
         'port' : 22,
         }
+        
     
     return switch_details
 
@@ -34,55 +54,79 @@ def switch_connect(switch_ip):
 #Grabs a config given a text file
 def grabconfigtxt(txt_file_name):
     switch_list = Get_Switch_List(f"{txt_file_name}.txt")
+    username = input("Username: ")
+    try:
+        password = getpass.getpass()
+    except Exception as err:
+        print("--------------------------")
+        print(err)
+        print("--------------------------")
 
     for switch_ip in switch_list:
         if not check_ip(switch_ip):
             print(f"IP {switch_ip} is invalid... Skipping...")
             continue
+        try:
+            net_connect = ConnectHandler(**switch_connect(switch_ip, username, password))
+            
+            switch_hostname = (net_connect.send_command('show conf | include hostname').split()[1])
+            conf_output = net_connect.send_command('show conf')
+            #print(conf_output)
+            
+            #File write
+            print(f"Working on: {switch_hostname}\n")
+            switch_txt = open(f"{switch_hostname}.txt", "w")
+            switch_txt.write(conf_output)
+            switch_txt.close()
+            print(f"Completed: {switch_hostname}\n")
+            
+            time.sleep(1) #As to not attempt to make every connection in half a second...
+            print("---------------------\n Finished")
+        except Exception as err:
+            print("--------------------------")
+            print(switch_ip)
+            print(err)
+            print("--------------------------")
+
         
+#Grabs a config given an ip address
+def grabconfigip(switch_ip):
+    try:
         net_connect = ConnectHandler(**switch_connect(switch_ip))
-        
+            
         switch_hostname = (net_connect.send_command('show conf | include hostname').split()[1])
         conf_output = net_connect.send_command('show conf')
         #print(conf_output)
         
-        #File write
+        #File write   
         print(f"Working on: {switch_hostname}\n")
         switch_txt = open(f"{switch_hostname}.txt", "w")
         switch_txt.write(conf_output)
         switch_txt.close()
         print(f"Completed: {switch_hostname}\n")
-        
-        time.sleep(1) #As to not attempt to make every connection in half a second...
+            
         print("---------------------\n Finished")
-        
-#Grabs a config given an ip address
-def grabconfigip(switch_ip):
-    net_connect = ConnectHandler(**switch_connect(switch_ip))
-        
-    switch_hostname = (net_connect.send_command('show conf | include hostname').split()[1])
-    conf_output = net_connect.send_command('show conf')
-    #print(conf_output)
+    except Exception as err:
+        print("--------------------------")
+        print(err)
+        print("--------------------------")
     
-    #File write   
-    print(f"Working on: {switch_hostname}\n")
-    switch_txt = open(f"{switch_hostname}.txt", "w")
-    switch_txt.write(conf_output)
-    switch_txt.close()
-    print(f"Completed: {switch_hostname}\n")
-        
-    print("---------------------\n Finished")
     
     
 #dumps config given the ip address
 def dumpconfig(switch_ip, txt_file_name):
-    net_connect = ConnectHandler(**switch_connect(switch_ip))
-    switch_hostname = (net_connect.send_command('show conf | include hostname').split()[1])
+    try:
+        net_connect = ConnectHandler(**switch_connect(switch_ip))
+        switch_hostname = (net_connect.send_command('show conf | include hostname').split()[1])
 
-    print(f"Dumping config on: {switch_hostname}\n")
-    net_connect.send_config_from_file(txt_file_name + ".txt")
-    net_connect.send_command("wr mem")
-    print(f"Config successfully dumped on: {switch_hostname}\n")
+        print(f"Dumping config on: {switch_hostname}\n")
+        net_connect.send_config_from_file(txt_file_name + ".txt")
+        net_connect.send_command("wr mem")
+        print(f"Config successfully dumped on: {switch_hostname}\n")
+    except Exception as err:
+        print("--------------------------")
+        print(err)
+        print("--------------------------")
 
 
 #Makes a list of ip addresses from a text file
